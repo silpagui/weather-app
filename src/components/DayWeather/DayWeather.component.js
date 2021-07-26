6a5369413a8db5d0c683710e56d5673d939f98c7
silpagui <silpagui.sa@gmail.com>
2021-07-26 17:53:36 +0200

    fix(css): add cross-icon to close-button

--- src/components/DayWeather/DayWeather.component.js
+++ src/components/DayWeather/DayWeather.component.js
@@ -1,6 +1,7 @@
 import "./DayWeather.styles.css";
 import LocationSRC from "../../assets/images/location.png";
 import gpsSRC from "../../assets/images/GPS-icon.png";
+import crossSRC from "../../assets/images/cross-icon.svg";
 import { SearchButton } from "../SearchButton/SearchButton.component";
 import { RoundButton } from "../RoundButton/RoundButton.component";
 import { useState } from "react";
@@ -50,7 +51,20 @@ export function DayWeather({
 
       <div className={`search-aside ${showSearchClass}`}>
         <button className="close-btn" onClick={handleOnClick}>
-          X
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            id="cross-icon"
+          >
+            <path
+              d="M13.2929 1.41L8.05645 6.64645L7.70289 7L8.05645 7.35355L13.2929 12.59L12.59 13.2929L7.35355 8.05645L7 7.70289L6.64645 8.05645L1.41 13.2929L0.707107 12.59L5.94355 7.35355L6.29711 7L5.94355 6.64645L0.707107 1.41L1.41 0.707107L6.64645 5.94355L7 6.29711L7.35355 5.94355L12.59 0.707107L13.2929 1.41Z"
+              fill="currentColor"
+              stroke="currentColor"
+            />
+          </svg>
         </button>
         <form className="search-location" onSubmit={handleOnSubmitSearch}>
           <input
