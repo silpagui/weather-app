0cc603e4c9c6e67eb7714d05da911c1c0fd922a5
silpagui <silpagui.sa@gmail.com>
2021-07-27 18:37:11 +0200

    ref(Cloud.component.js): enhance logic

--- src/components/Cloud/Cloud.component.js
+++ src/components/Cloud/Cloud.component.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Cloud.styles.css";
 
 export function Cloud({ className }) {
-  const cloudsCircle = [1, 2, 3, 4];
+  const cloudsCircle = Array.from(new Array(4));
 
   return (
     <span className={`cloud-container ${className}`}>
