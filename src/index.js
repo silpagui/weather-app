a3e35a42410f2186f77a9ebafe5b7483555a3c97
silpagui <silpagui.sa@gmail.com>
2021-07-27 18:34:14 +0200

    fix(testing.js): remove unnecessary folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App/App.component.js";
-// import { Testing } from "./components/Testing/Testing.component.js";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
